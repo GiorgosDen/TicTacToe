0de26ea700af1343d558de39413b1cc7a076a5e0
GiorgosDen <75185790+GiorgosDen@users.noreply.github.com>
2023-03-12 20:44:13 +0200

    Flex try 2
    2sd Flexbox try

--- CentralPage/Actions.js
+++ CentralPage/Actions.js
@@ -1,8 +1,22 @@
+
+
 function printInfo(){
-    document.getElementById('infoPanel').style.animationName= "showMenu"; 
+    document.getElementById('infoPanel').style.animationName= "showMenu";
+    document.getElementById('menu').style.animationDuration="1s"; 
+    document.getElementById('menu').style.animationName="closeMenu";
 }
 
 function closeInfo(){
-    document.getElementById('infoPanel').style.animationName= "closeMenu";    
+    document.getElementById('infoPanel').style.animationName= "closeMenu";  
+    document.getElementById('menu').style.animationDuration="3s";   
+    document.getElementById('menu').style.animationName="showMenu";
+}
+
+function markButton(b){
+    document.getElementById(b).style.backgroundColor="rgb(205,133,63)";
+}
+
+function normalButton(b){
+    document.getElementById(b).style.backgroundColor = "rgb(155, 125, 95)";
 }
 
